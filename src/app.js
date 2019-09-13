1797895951f3a2dfa3723cb137ac10b00998e9f2
Oskar-Hellkvist <oh222gw@student.lnu.se>
2019-09-13 02:02:45 +0200

    finished frontend, will have to work on scaling issues though

--- src/app.js
+++ src/app.js
@@ -7,44 +7,96 @@ let query
 
 form.addEventListener('submit', formSubmitted)
 
+window.onload = formSubmitted
+
 function formSubmitted(event){
     event.preventDefault()
-
-    // console.log(`${BASE_URL}${query}`)
-    getSchedule(query).then(res => {
-        displayData(res)
-    })
+    if(typeof(query) === 'undefined'){
+        getSchedule('today').then(res => {
+            displayData(res)
+        })
+    } else {
+        getSchedule(query).then(res => {
+            displayData(res)
+        })
+    }
 }
 
 
-function getSchedule(query){
+function getSchedule(query){    
     return fetch(`${BASE_URL}${query}`)
     .then(res => res.json())
 }
 
 
 function displayData(results){
+    main.innerHTML = ""
     if(results.length > 1){
         let lectures = []
+
         for(let i = 0; i < results.length; i++){
+
+            const borderWrapper = document.createElement('div')
+            const dayDiv = document.createElement('div')
+            const dayDateHeader = document.createElement('h3')
+
+            borderWrapper.className = 'border-gradient-wrapper'
+            dayDiv.className = 'date-schedule'
+            dayDateHeader.className = 'date'
+
             let day = results[i]
             let dayDate = day.date
-            console.log(dayDate)
+            dayDateHeader.innerHTML = dayDate
+            dayDiv.appendChild(dayDateHeader)
             let keys = Object.keys(day)
+            
             for(let j = 1; j < keys.length; j++){
+
+                const dayLecture = document.createElement('div')
+                dayLecture.className = 'lecture'
+
                 let lecture = keys[j]
                 let lectureTime = day[lecture].time
                 let lectureCourse = day[lecture].course
                 let lectureRoom = day[lecture].room
                 let lectureActivity = day[lecture].activity
                 let lectureGroup = day[lecture].group
-                console.log(`${lecture}-${lectureTime}-${lectureCourse}-${lectureRoom}-${lectureActivity}-${lectureGroup}`)
+
+                const time = document.createElement('p')
+                time.className = 'time'
+                time.innerHTML = `<b>Time: </b>${lectureTime}`
+                const course = document.createElement('p')
+                course.className = 'course'
+                course.innerHTML = `<b>Course: </b>${lectureCourse}`
+                const room = document.createElement('p')
+                room.className = 'room'
+                room.innerHTML = `<b>Room: </b>${lectureRoom}`
+                const activity = document.createElement('p')
+                activity.className = 'activity'
+                activity.innerHTML = `<b>Activity: </b>${lectureActivity}`
+                const group = document.createElement('p')
+                group.className = 'group'
+                group.innerHTML = `<b>Group: </b>${lectureGroup}`
+
+                dayLecture.appendChild(time)
+                dayLecture.appendChild(activity)
+                dayLecture.appendChild(room)
+                dayLecture.appendChild(group)
+                dayLecture.appendChild(course)
+
+
+            dayDiv.appendChild(dayLecture)
             }
+            borderWrapper.appendChild(dayDiv)
+            main.appendChild(borderWrapper)
         }
     } else {
-        console.log(results)
+        const borderWrapper = document.createElement('div')
+        const empty = document.createElement('h2')
+        borderWrapper.className = 'empty-border-gradient-wrapper'
+        empty.className = 'empty-day'
+        empty.innerHTML = results.message
+        borderWrapper.appendChild(empty)
+        main.appendChild(borderWrapper)
     }
 }
-
-
-
